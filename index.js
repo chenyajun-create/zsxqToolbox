5b82da4e148fb7e74a183f1f122f11f6e327118c
陈亚军 <877991079@qq.com>
2023-07-31 10:11:15 +0800

    优化弹出框出现时隐藏最外层滚动条

--- index.js
+++ index.js
@@ -264,16 +264,48 @@
   document.body.appendChild(overlayDom)
 
   // 隐藏遮罩层
+  class OverlayStyle {
+    // 显示弹出框
+    showOverlayDom() {
+      overlayDom.style.display = 'block'
+      document.body.style.overflowY = 'hidden'
+    }
+    // 显示弹出框
+    hiddenOverlayDom() {
+      overlayDom.style.display = 'none'
+      document.body.style.overflowY = 'scroll'
+    }
+  }
+  const overlayState = new OverlayStyle()
   const overlay = overlayDom.querySelector('#overlay')
   const closeDom = overlayDom.querySelector('.closeIcon')
   const cancelDom = overlayDom.querySelector('.cancelOverlay')
-  overlay.addEventListener('click', hideOverlay)
-  closeDom.addEventListener('click', hideOverlay)
-  cancelDom.addEventListener('click', hideOverlay)
-  function hideOverlay() {
-    overlayDom.style.display = 'none'
-  }
+  overlay.addEventListener('click', overlayState.hiddenOverlayDom)
+  closeDom.addEventListener('click', overlayState.hiddenOverlayDom)
+  cancelDom.addEventListener('click', overlayState.hiddenOverlayDom)
+  //#endregion
 
+  //#region 设置icon
+  // 创建设置icon图标
+  const settingDom = document.createElement('div')
+  settingDom.addEventListener('click', overlayState.showOverlayDom)
+
+  settingDom.style.cursor = 'pointer'
+  settingDom.style.height = '37px'
+  settingDom.innerHTML = `
+  <svg t="1690013293428" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2268" width="37" height="37">
+  <path d="M439.264 208a16 16 0 0 0-16 16v67.968a239.744 239.744 0 0 0-46.496 26.896l-58.912-34a16 16 0 0 0-21.856 5.856l-80 138.56a16 16 0 0 0 5.856 21.856l58.896 34a242.624 242.624 0 0 0 0 53.728l-58.88 34a16 16 0 0 0-6.72 20.176l0.848 1.68 80 138.56a16 16 0 0 0 21.856 5.856l58.912-34a239.744 239.744 0 0 0 46.496 26.88V800a16 16 0 0 0 16 16h160a16 16 0 0 0 16-16v-67.968a239.744 239.744 0 0 0 46.512-26.896l58.912 34a16 16 0 0 0 21.856-5.856l80-138.56a16 16 0 0 0-4.288-20.832l-1.568-1.024-58.896-34a242.624 242.624 0 0 0 0-53.728l58.88-34a16 16 0 0 0 6.72-20.176l-0.848-1.68-80-138.56a16 16 0 0 0-21.856-5.856l-58.912 34a239.744 239.744 0 0 0-46.496-26.88V224a16 16 0 0 0-16-16h-160z m32 48h96v67.376l28.8 12.576c13.152 5.76 25.632 12.976 37.184 21.52l25.28 18.688 58.448-33.728 48 83.136-58.368 33.68 3.472 31.2a194.624 194.624 0 0 1 0 43.104l-3.472 31.2 58.368 33.68-48 83.136-58.432-33.728-25.296 18.688c-11.552 8.544-24.032 15.76-37.184 21.52l-28.8 12.576V768h-96v-67.376l-28.784-12.576c-13.152-5.76-25.632-12.976-37.184-21.52l-25.28-18.688-58.448 33.728-48-83.136 58.368-33.68-3.472-31.2a194.624 194.624 0 0 1 0-43.104l3.472-31.2-58.368-33.68 48-83.136 58.432 33.728 25.296-18.688a191.744 191.744 0 0 1 37.184-21.52l28.8-12.576V256z m47.28 144a112 112 0 1 0 0 224 112 112 0 0 0 0-224z m0 48a64 64 0 1 1 0 128 64 64 0 0 1 0-128z" fill="#c5c6cb" p-id="2269">
+  </path></svg>
+  `
+
+  // 等待icon容器加载结束添加
+  waitForElm('.user-container').then((elm) => {
+    creatSettingIcon(elm) //添加设置icon
+  })
+  // 创建设置icon
+  function creatSettingIcon(element) {
+    element.appendChild(settingDom)
+  }
   //#endregion
 
   //#region tabs
@@ -577,31 +609,6 @@
     observer.observe(target, {
       childList: true,
     })
-  }
-  //#endregion
-
-  //#region 设置icon
-  // 创建设置icon图标
-  const settingDom = document.createElement('div')
-  settingDom.addEventListener('click', () => {
-    overlayDom.style.display = 'block'
-  })
-
-  settingDom.style.cursor = 'pointer'
-  settingDom.style.height = '37px'
-  settingDom.innerHTML = `
-  <svg t="1690013293428" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2268" width="37" height="37">
-  <path d="M439.264 208a16 16 0 0 0-16 16v67.968a239.744 239.744 0 0 0-46.496 26.896l-58.912-34a16 16 0 0 0-21.856 5.856l-80 138.56a16 16 0 0 0 5.856 21.856l58.896 34a242.624 242.624 0 0 0 0 53.728l-58.88 34a16 16 0 0 0-6.72 20.176l0.848 1.68 80 138.56a16 16 0 0 0 21.856 5.856l58.912-34a239.744 239.744 0 0 0 46.496 26.88V800a16 16 0 0 0 16 16h160a16 16 0 0 0 16-16v-67.968a239.744 239.744 0 0 0 46.512-26.896l58.912 34a16 16 0 0 0 21.856-5.856l80-138.56a16 16 0 0 0-4.288-20.832l-1.568-1.024-58.896-34a242.624 242.624 0 0 0 0-53.728l58.88-34a16 16 0 0 0 6.72-20.176l-0.848-1.68-80-138.56a16 16 0 0 0-21.856-5.856l-58.912 34a239.744 239.744 0 0 0-46.496-26.88V224a16 16 0 0 0-16-16h-160z m32 48h96v67.376l28.8 12.576c13.152 5.76 25.632 12.976 37.184 21.52l25.28 18.688 58.448-33.728 48 83.136-58.368 33.68 3.472 31.2a194.624 194.624 0 0 1 0 43.104l-3.472 31.2 58.368 33.68-48 83.136-58.432-33.728-25.296 18.688c-11.552 8.544-24.032 15.76-37.184 21.52l-28.8 12.576V768h-96v-67.376l-28.784-12.576c-13.152-5.76-25.632-12.976-37.184-21.52l-25.28-18.688-58.448 33.728-48-83.136 58.368-33.68-3.472-31.2a194.624 194.624 0 0 1 0-43.104l3.472-31.2-58.368-33.68 48-83.136 58.432 33.728 25.296-18.688a191.744 191.744 0 0 1 37.184-21.52l28.8-12.576V256z m47.28 144a112 112 0 1 0 0 224 112 112 0 0 0 0-224z m0 48a64 64 0 1 1 0 128 64 64 0 0 1 0-128z" fill="#c5c6cb" p-id="2269">
-  </path></svg>
-  `
-
-  // 等待icon容器加载结束添加
-  waitForElm('.user-container').then((elm) => {
-    creatSettingIcon(elm) //添加设置icon
-  })
-  // 创建设置icon
-  function creatSettingIcon(element) {
-    element.appendChild(settingDom)
   }
   //#endregion
 
